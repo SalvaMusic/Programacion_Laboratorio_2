7594db9e0d5b3b486cde66498bcdc1d61fa86ae8
SalvaMusic <salva.7693@gmail.com>
2018-10-19 23:20:57 -0300

    Agrega ejercicio 30

---
+++ Ejercicio 30/Entidades/AutoF1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class AutoF1
+    {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private int numero;
+        private short vueltasRestantes;
+
+        public AutoF1(int numero, string escuderia)
+        {
+            this.cantidadCombustible = 0;
+            this.vueltasRestantes = 0;
+            this.enCompetencia = false;
+
+            this.numero = numero;
+            this.escuderia = escuderia;
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder str = new StringBuilder();
+            string estado = "";
+
+            if (this.enCompetencia)
+            { estado = "En Competencia"; }
+            else
+            { estado = "Sin Competir"; }
+
+            str.AppendFormat("Nro: {0}\tEscuderia: {1}\nEstado: {2}",this.numero,this.escuderia,estado);
+
+            return str.ToString();
+        }
+
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            if ((a1.escuderia == a2.escuderia) && (a1.numero == a2.numero))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1 == a2);
+        }
+
+        public short CantidadCombustible
+        {
+            get
+            {
+                return this.cantidadCombustible;
+            }
+            set
+            {
+                this.cantidadCombustible = value;
+            }
+        }
+
+
+        public short VueltasRestantes
+        {
+            get
+            {
+                return this.vueltasRestantes;
+            }
+            set
+            {
+                this.vueltasRestantes = value;
+            }
+        }
+
+        public bool EnCompetencia
+        {
+            get
+            {
+                return this.enCompetencia;
+            }
+            set
+            {
+                this.enCompetencia = value;
+            }
+        }
+
+
+    }
+}
---
+++ Ejercicio 30/Entidades/Competencia.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadDeVueltas;
+        private List<AutoF1> listaAutosF1;
+
+        private Competencia()
+        {
+            listaAutosF1 = new List<AutoF1>();
+        }
+
+        public Competencia(short cantVueltas, short cantCompetidores)
+            : this()
+        {
+            this.cantidadDeVueltas = cantVueltas;
+            this.cantidadCompetidores = cantCompetidores;
+        }
+
+
+        public string MostrarDatos()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendFormat("Cantidad de Competidores: {0}\tCantidad de Vueltas: {1}", this.cantidadCompetidores, this.cantidadDeVueltas);
+
+            foreach(AutoF1 auto in this.listaAutosF1)
+            {
+                str.AppendLine();
+                str.Append(auto.MostrarDatos());
+            }
+            str.AppendLine("\n");
+
+            return str.ToString();
+        }
+
+        public static bool operator ==(Competencia c, AutoF1 a)
+        {
+            foreach (AutoF1 auto in c.listaAutosF1)
+            {
+                if (auto == a)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Competencia c, AutoF1 a)
+        {
+            return !(c == a);
+        }
+
+        public static bool operator +(Competencia c, AutoF1 a)
+        {
+            bool retorno = false;
+            if((c.listaAutosF1.Count < c.cantidadCompetidores) && (c != a))
+            {
+                c.listaAutosF1.Add(a);
+                Random rnd = new Random();
+                rnd.Next(15, 100);
+                a.EnCompetencia = true;
+                a.VueltasRestantes = c.cantidadDeVueltas;
+                a.CantidadCombustible = (short)rnd.Next(15, 100);
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator -(Competencia c, AutoF1 a)
+        {
+            bool retorno = false;
+
+            if (c.listaAutosF1.Contains(a))
+            {
+                retorno = true;
+                a.EnCompetencia = false;
+                a.VueltasRestantes = 0;
+                a.CantidadCombustible = 0;
+                c.listaAutosF1.Remove(a);
+            }
+
+            return retorno;
+
+        }
+
+    }
+}
